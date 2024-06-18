0b270ef1a5ca98862ae6a9f8f3de368c9b95082a
Brandocean <brandocean2016@gmail.com>
2024-06-17 19:20:29 -0600

    Delete ticket

--- components/DeleteBlock.jsx
+++ components/DeleteBlock.jsx
@@ -1,9 +1,30 @@
+"use client"
+
 import { faX } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useRouter } from "next/navigation";
 
-const DeleteBlock = () => {
+const DeleteBlock = ({ id }) => {
+
+  const apiUrl = "http://localhost:3000";
+
+  const router = useRouter();
+
+  const deleteTicket = async () => {
+    const res = await fetch(`${apiUrl}/api/Tickets/${id}`, {
+      method: "DELETE",
+    });
+    if (res.ok) {
+      router.refresh();
+    }
+  }
+
   return (
-    <FontAwesomeIcon icon={faX} className="text-red-400 hover:cursor-pointer hover:text-red-200" />
+    <FontAwesomeIcon
+      icon={faX}
+      className="text-red-400 hover:cursor-pointer hover:text-red-200"
+      onClick={deleteTicket}
+    />
   )
 }
 

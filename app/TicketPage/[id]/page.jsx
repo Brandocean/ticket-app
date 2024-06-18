a9a48dd15e65ff7f09e7ec3aefeadadf5d8e39dd
Brandocean <brandocean2016@gmail.com>
2024-06-17 19:58:16 -0600

    Add first part of EditMode

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,6 +1,31 @@
 import TicketForm from "@/components/TicketForm"
 
-const TicketPage = ({ params }) => {
+const getTicketById = async (id) => {
+
+  const apiUrl = "http://localhost:3000";
+
+  const res = await fetch(`${apiUrl}/api/Tickets/${id}`, {
+    cache: "no-store"
+  })
+
+  if (!res.ok) {
+    throw new Error("Failed to get ticket.");
+  }
+
+  return res.json();
+}
+
+
+const TicketPage = async ({ params }) => {
+
+  const EDITMODE = params.id === "new" ? false : true;
+  let updateTicketData = {};
+
+  if (EDITMODE) {
+    updateTicketData = await getTicketById(params.id);
+    console.log(updateTicketData);
+  }
+
   return (
     <TicketForm />
   )
